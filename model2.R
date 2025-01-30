5db2054151d3f099bd589341662789ae40ecd2ef
Laura-BN <lbuitrago41@gmail.com>
2025-01-29 19:25:17 -0500

    Update model2.R

--- model2.R
+++ model2.R
@@ -1,2 +1,6 @@
 
 # Fill here the code to estimate the linear_model2 from the notebook
+# re run the model
+linear_model2<- lm(totalHoursWorked ~ ofic_ingLab + nmenores  +  nmenores*gender + H_Head + age + gender, data=db_int2  )
+# el numero de niños crecio, el efecto de ingreso ocupados también (entre (1) y (2))
+
