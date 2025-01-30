8a3d0cd5ae862f048d9c4157c6f13aa09c1df216
MayaGutiBan <gubita@icloud.com>
2025-01-29 19:26:52 -0500

    Update model3.R

--- model3.R
+++ model3.R
@@ -1,1 +1,18 @@
 # Fill here the code to estimate the linear_model3 from the notebook
+c<-ggplot(data= db_int, 
+          mapping = aes(y=totalHoursWorked, x="")) +
+  theme_bw() +
+  geom_boxplot()  +
+  ggtitle("")+
+  ylab("Total Hours Worked")+
+  xlab("")
+
+low <- mean(db_int$totalHoursWorked) - 2* sd(db_int$totalHoursWorked)
+up <- mean(db_int$totalHoursWorked) + 2* sd(db_int$totalHoursWorked)
+grid.arrange(b, c, ncol = 2)
+
+db_int<- db_int %>% mutate(out_totalHoursWorked=  ifelse( test=(totalHoursWorked< low | totalHoursWorked> up), 
+                                                          yes= 1,
+                                                          no= 0))
+c <- c + geom_hline(yintercept = low,linetype="solid",color="red",size=0.7) +
+  geom_hline(yintercept = up,linetype="solid",color="red",size=0.7)
